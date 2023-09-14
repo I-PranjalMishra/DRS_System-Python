fb9a2aded0403f3b1970b1ef69a290806b29e01b
I-PranjalMishra <pranjal07mishra@gmail.com>
2023-09-14 23:09:39 +0530

    Uploading

---
+++ main.py
@@ -0,0 +1,106 @@
+import tkinter 
+import cv2
+import PIL.Image, PIL.ImageTk 
+from functools import partial
+import threading
+import time
+import imutils 
+
+stream = cv2.VideoCapture("Images/video.mp4")
+flag = True
+def play(speed):
+    global flag
+
+    # Play the video in reverse mode
+    frame1 = stream.get(cv2.CAP_PROP_POS_FRAMES)
+    stream.set(cv2.CAP_PROP_POS_FRAMES, frame1 + speed)
+
+    grabbed, frame = stream.read()
+    if not grabbed:
+        exit()
+    frame = imutils.resize(frame, width=SET_WIDTH, height=SET_HEIGHT)
+    frame = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
+    canvas.image = frame
+    canvas.create_image(0,0, image=frame, anchor=tkinter.NW)
+    if flag:
+        canvas.create_text(134, 26, fill="black", font="Times 26 bold", text="Decision Pending")
+    flag = not flag
+    
+
+def pending(decision):
+    # 1. Display decision pending image
+    frame = cv2.cvtColor(cv2.imread("Images/Decision Pending.png"), cv2.COLOR_BGR2RGB)
+    frame = imutils.resize(frame, width=SET_WIDTH, height=SET_HEIGHT)
+    frame = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+    canvas.image = frame
+    canvas.create_image(0,0, image=frame, anchor=tkinter.NW)
+    # 2. Wait for 1 second
+    time.sleep(1.5)
+
+    # 3. Display Codeplay image
+    frame = cv2.cvtColor(cv2.imread("Images/codeplay.png"), cv2.COLOR_BGR2RGB)
+    frame = imutils.resize(frame, width=SET_WIDTH, height=SET_HEIGHT)
+    frame = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+    canvas.image = frame
+    canvas.create_image(0,0, image=frame, anchor=tkinter.NW)
+
+    # 4. Wait for 1.5 second
+    time.sleep(2.5)
+    # 5. Display out/notout image
+    if decision == 'out':
+        decisionImg = "Images/OUT.png"
+    else:
+        decisionImg = "Images/NOT OUT.png"
+    frame = cv2.cvtColor(cv2.imread(decisionImg), cv2.COLOR_BGR2RGB)
+    frame = imutils.resize(frame, width=SET_WIDTH, height=SET_HEIGHT)
+    frame = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+    canvas.image = frame
+    canvas.create_image(0,0, image=frame, anchor=tkinter.NW)
+
+
+def out():
+    thread = threading.Thread(target=pending, args=("out",))
+    thread.daemon = 1
+    thread.start()
+    
+
+
+def not_out():
+    thread = threading.Thread(target=pending, args=("not out",))
+    thread.daemon = 1
+    thread.start()
+    
+# Width and height of our main screen
+SET_WIDTH = 650
+SET_HEIGHT = 368
+
+# Tkinter gui starts here
+window = tkinter.Tk()
+window.title("Third Umpire Decision Review Kit")
+cv_img = cv2.cvtColor(cv2.imread("Images/Drs.png"), cv2.COLOR_BGR2RGB)
+cv_img = imutils.resize(cv_img, width=SET_WIDTH, height=SET_HEIGHT)
+canvas = tkinter.Canvas(window, width=SET_WIDTH, height=SET_HEIGHT)
+photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(cv_img))
+image_on_canvas = canvas.create_image(0, 0, ancho=tkinter.NW, image=photo)
+canvas.pack()
+
+
+# Buttons to control playback
+btn = tkinter.Button(window, text="<< Previous (fast)", width=50, command=partial(play, -25))
+btn.pack()
+
+btn = tkinter.Button(window, text="<< Previous (slow)", width=50, command=partial(play, -2))
+btn.pack()
+
+btn = tkinter.Button(window, text="Next (slow) >>", width=50, command=partial(play, .2))
+btn.pack()
+
+btn = tkinter.Button(window, text="Next (fast) >>", width=50, command=partial(play, 25))
+btn.pack()
+
+btn = tkinter.Button(window, text="Give Out", width=50, command=out)
+btn.pack()
+
+btn = tkinter.Button(window, text="Give Not Out", width=50, command=not_out)
+btn.pack()
+window.mainloop()
